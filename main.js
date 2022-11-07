67ea1ea828e83a0de9365d178235a95a363f7d7d
krishankanhaya <krishankanhaya0007@gmail.com>
2022-11-07 20:09:48 +0530

    file is added

---
+++ main.js
@@ -0,0 +1,12 @@
+
+const deck = ['RD1','RD2','RD3','RD4','RD5','RD6','RD7','RD8','RD9','RDA','RDJ','RDK','RDQ',
+'RH1','RH2','RH3','RH4','RH5','RH6','RH7','RH8','RH9','RHA','RHJ','RHK','RHQ',
+'BS1','BS2','BS3','BS4','BS5','BS6','BS7','BS8','BS9','BSA','BSJ','BSK','BSQ',
+'BC1','BC2','BC3','BC4','BC5','BC6','BC7','BC8','BC9','BCA','BCJ','BCK','BCQ'];
+
+console.log(...deck);
+for(let j=deck.length-1;j>0;j--)           {
+let val = Math.floor(Math.random()*(j+1));
+[deck[j],deck[val]] = [deck[val],deck[j]];
+}
+console.log(...deck);
